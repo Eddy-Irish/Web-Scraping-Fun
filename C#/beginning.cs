506e1872acabc76b4c5ca242a0432bf73bea2eb3
Keegan <keeganohara7@gmail.com>
2018-06-26 10:15:36 -0400

    Update beginning.cs

--- C#/beginning.cs
+++ C#/beginning.cs
@@ -1,3 +1,2 @@
-//This will eventually be a C# application for web scraping
-//various data from random sites just screaming to have 
-//their random data scraped and stared at
+//This is still in its infancy (and I'm just having fun with it), and is a mess of practice code
+// so I haven't committed anything up yet.   
